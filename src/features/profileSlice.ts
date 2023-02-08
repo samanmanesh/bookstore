787a09c28ca37a93860318a183ef87ed52b4b924
Saman <samanmanesh7@gmail.com>
2023-02-08 16:11:38 -0500

    profile state works

--- src/features/profileSlice.ts
+++ src/features/profileSlice.ts
@@ -1,6 +1,7 @@
+import { RootState } from "@/app/store";
 import { createSlice } from "@reduxjs/toolkit";
 
-interface ProfileState {
+export interface ProfileState {
   firstName: string;
   lastName: string;
   email: string;
@@ -22,37 +23,45 @@ export const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    setFirstName: (state, action) => {
-      state.firstName = action.payload;
+    updateProfile : (state, action) => {
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+      state.email = action.payload.email;
+      state.phone = action.payload.phone;
+      state.blingStatus = action.payload.blingStatus;
+      state.profileImage = action.payload.profileImage;
     }
-    ,
-    setLastName: (state, action) => {
-      state.lastName = action.payload;
-    }
-    ,
-    setEmail: (state, action) => {
-      state.email = action.payload;
-    }
-    ,
-    setPhone: (state, action) => {
-      state.phone = action.payload;
-    }
-    ,
-    setBlingStatus: (state, action) => {
-      state.blingStatus = action.payload;
-    }
-    ,
-    setProfileImage: (state, action) => {
-      state.profileImage = action.payload;
-    }
-
   },
 });
 
 //write the type of the actions
-export const { setFirstName, setLastName, setEmail, setPhone, setBlingStatus, setProfileImage } = profileSlice.actions;
-
+export const { updateProfile} = profileSlice.actions;
+export const selectProfile = (state: RootState) => state.profile;
 export default profileSlice.reducer;  
     
+
+// setFirstName: (state, action) => {
+//   state.firstName = action.payload;
+// }
+// ,
+// setLastName: (state, action) => {
+//   state.lastName = action.payload;
+// }
+// ,
+// setEmail: (state, action) => {
+//   state.email = action.payload;
+// }
+// ,
+// setPhone: (state, action) => {
+//   state.phone = action.payload;
+// }
+// ,
+// setBlingStatus: (state, action) => {
+//   state.blingStatus = action.payload;
+// }
+// ,
+// setProfileImage: (state, action) => {
+//   state.profileImage = action.payload;
+// }
 
 
--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,20 +1,38 @@
-import React from "react";
+import { useState,useEffect } from "react";
 import Image from "next/image";
 import { useSelector, useDispatch } from "react-redux";
+import {updateProfile, ProfileState, selectProfile} from  '../features/profileSlice';
 
 const Account = () => {
-  return (
-    <div className="flex place-items-center h-full my-32 px-16 place-content-center bg-red-400  space-x-24 ">
-      <div className="flex flex-col place-items-center justify-center  ">
-        <div className="w-64 h-64 rounded-full text-[#EAEBED] bg-[#2E424D] flex place-items-center justify-center text-lg">
-          S <Image src={""} alt="S" className="w-full h-full rounded-full" />
-        </div>
+  const data = useSelector(selectProfile);
+  const dispatch = useDispatch();
+  const [localData, setLocalData] = useState<ProfileState>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    blingStatus: "",
+    profileImage: "",
+  });
 
+  useEffect (() => {
+    setLocalData(data)
+  }, [data])
+
+  console.log("profileState", data)
+
+  
+  return (
+    <div className="flex place-items-center h-full my-32 px-16 place-content-center space-x-24 ">
+      <div className="flex flex-col place-items-center justify-center  ">
+        <div className="w-64 h-64 rounded-full text-[#EAEBED] bg-[#2E424D] flex place-items-center justify-center text-4xl text-bold ">
+          {localData.profileImage?  <Image src={localData.profileImage} alt="S" className="w-full h-full rounded-full" />
+          : localData.firstName[0] + localData.lastName[0]}
+        </div>
         <button className="bg-gray-700 text-white rounded p-4 mt-12">
           Update Image
         </button>
       </div>
-
       <div className=" w-1/3 h-full ">
         <div className="flex flex-col space-y-8  ">
           <div className="flex items-center justify-between  ">
