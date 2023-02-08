bdecd6122ca0ff8aa3237612f020e294257a3684
Saman <samanmanesh7@gmail.com>
2023-02-08 13:39:00 -0500

    added react redux and redux toolkit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,23 +8,35 @@ function Header() {
   return (
     <div className="sticky top-0 inset-x-0   flex  px-16 py-4 justify-between place-items-center text-lg  font-medium  ">
       <ul className="flex justify-items-center justify-evenly space-x-8 ">
-        <li
-        >
-          <Link   className={`${
-            currentPath === "/" && "border-b-2 border-black "
-          } px-2 py-1 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm`} href="/">Home</Link>
+        <li>
+          <Link
+            className={`${
+              currentPath === "/" && "border-b-2 border-black "
+            } px-2 py-2 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm`}
+            href="/"
+          >
+            Home
+          </Link>
         </li>
-        <li
-        >
-          <Link   className={` px-2 py-1  duration-400 ${
-            currentPath === "/products" && "border-b-2 border-black  "
-          } px-2 py-1 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm`} href="/products">Products</Link>
+        <li>
+          <Link
+            className={` px-2 py-1  duration-400 ${
+              currentPath === "/products" && "border-b-2 border-black  "
+            } px-2 py-2 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm`}
+            href="/products"
+          >
+            Products
+          </Link>
         </li>
-        <li
-        >
-          <Link className={`duration-400 ${
-            currentPath === "/cart" && "border-b-2 border-black"
-          } px-2 py-1 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm `} href="/cart">Cart</Link>
+        <li>
+          <Link
+            className={`duration-400 ${
+              currentPath === "/cart" && "border-b-2 border-black"
+            } px-2 py-2 hover:bg-[#2E424D] hover:text-white duration-300 rounded-sm `}
+            href="/cart"
+          >
+            Cart
+          </Link>
         </li>
       </ul>
 
